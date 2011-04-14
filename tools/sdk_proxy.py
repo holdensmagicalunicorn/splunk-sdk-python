5b83ada0b3be1bccb999e1b8980f2da6750ba0af
Wim Colgate <wcolgate@splunk.com>
2011-04-14 13:33:14 -0700

    Incremental checkin, capture a jobs request from a browser or BI app and redirect to a POST and wait on the response and put back using Atom.
    Clean up tracing too.

--- tools/sdk_proxy.py
+++ tools/sdk_proxy.py
@@ -28,10 +28,12 @@ import os.path
 import os
 import sys
 import urllib
+import time
 import xml.dom.minidom
 
 # splunk support files
 import tools.cmdopts as cmdopts
+import splunk.binding as binding
 from splunk.binding import connect
 
 DEBUG = True
@@ -58,10 +60,145 @@ FD = None
 if DEBUG:
     FD = open('./sdk_proxy.debug', 'w')
 
+def trace(string):
+    """ trace something to the log file, if debug is on """
+    if DEBUG:
+        FD.write(string + "\n")
+        FD.flush()
+
+def convert_xml_to_atom(xml_text):
+    """ splunk specific XML to Atom coverter """
+
+    ## this function takes a splunk search XML and converts it
+    ## into Atom. A distilled Atom description can be found here:
+    ##
+    ## http://www.atomenabled.org/developers/syndication/
+    ##
+    ## for the full RFC:
+    ##
+    ## http://tools.ietf.org/html/rfc4287
+    ##
+
+    ## Requires elements of <feed>:
+    ## id:      Identifies the feed using a universally unique and permanent 
+    ##          URI. If you have a long-term, renewable lease on your Internet
+    ##          domain name, then use your website's address.
+    ##          <id>http://host-name:8086</id>
+    ## title:   Contains a human readable title for the feed. 
+    ##          <title>Splunk event report</title>
+    ## updated: Indicates the last time the feed was modified significantly.
+    ##          <updated>2003-12-13T18:30:02Z</updated>
+    ##
+
+    ## Recommended elements of <feed>:
+    ## author:  Names one author of the feed. 
+    ##          <author>
+    ##            <name>Splunk</name>
+    ##          </author>
+    ## link:    Identifies a related Web page. 
+    ##          The type of relation is defined by the rel attribute. 
+    ##          A feed is limited to one alternate per type and hreflang. 
+    ##          A feed should contain a link back to the feed itself.
+    ##          <link rel="self" href="/feed" />
+    ##
+
+    ## Optional elements of <feed>:
+    ## category:Specifies a category that the feed belongs to. 
+    ##          A feed may have multiple category elements.
+    ##          <category term="sports"/>
+    ## contributor: Names one contributor to the feed. 
+    ##          An feed may have multiple contributor elements. 
+    ##          <contributor>
+    ##            <name>Jane Doe</name>
+    ##          </contributor>
+    ## generator: Identifies the software used to generate the feed, 
+    ##          for debugging and other purposes. 
+    ##          Both the uri and version attributes are optional.
+    ##          <generator uri="/myblog.php" version="1.0">
+    ##            Example Toolkit
+    ##          </generator>
+    ## icon:    Identifies a small image which provides iconic visual 
+    ##          identification for the feed. Icons should be square.
+    ##          <icon>/icon.jpg</icon>
+    ## logo:    Identifies a larger image which provides visual identification 
+    ##          for the feed. Images should be twice as wide as they are tall.
+    ##          <logo>/logo.jpg</logo>
+    ## rights:  Conveys information about rights, e.g. copyrights, held in 
+    ##          and over the feed.
+    ##          <rights type="html">
+    ##            &amp;copy; 2005 John Doe
+    ##          </rights>
+    ## subtitle:Contains a human-readable description or subtitle of the feed. 
+    ##          <subtitle>all your examples are belong to us</subtitle>`
+    ##
+
+    ## ****************************************************************
+
+    ## Required elements of <entry>
+    ## id:      Identifies the entry using a universally unique and 
+    ##          permanent URI. Two entries in a feed can have the same value 
+    ##          for id if they represent the same entry at different 
+    ##          points in time.
+    ##          <id>http://host-name:8086/xxx/yyyy</id>
+    ## title:   Contains a human readable title for the entry.
+    ##          <title>Splunk powered nanomites scour your IT</title>
+    ## updated	Indicates the last time the entry was modified.
+    ##          <updated>2003-12-13T18:30:02-05:00</updated>
+    ##
+
+    ## Recommended elements of <entry>
+    ## author	Names one author of the entry.
+    ##          <author>
+    ##            <name>Splunk</name>
+    ##          </author>
+    ## content: Contains or links to the complete content of the entry. 
+    ##          Content must be provided if there is no alternate link, 
+    ##          and should be provided if there is no summary. 
+    ##          <content>all the relevant information of this entry </content>
+    ## link:    Identifies a related Web page. The type of relation is defined 
+    ##          by the rel attribute. An entry is limited to one alternate per 
+    ##          type and hreflang. An entry must contain an alternate link if 
+    ##          there is no content element.
+    ##          <link rel="alternate" href="/blog/1234"/>
+    ## summary: Conveys a short summary, abstract, or excerpt of the entry. 
+    ##          Summary should be provided if there either is no content 
+    ##          provided for the entry, or that content is not inline 
+    ##          (i.e., contains a src attribute), or if the content is encoded 
+    ##          in base64.
+    ##          <summary>Some text.</summary>
+    ##
+
+    ## Optional elements of <entry>
+    ## category:Specifies a category that the entry belongs to. An entry may 
+    ##          have multiple category elements.
+    ##          <category term="data"/>
+    ## contributor:Names one contributor to the entry. An entry may have 
+    ##          multiple contributor elements. 
+    ##          <contributor>
+    ##            <name>Monty Python</name>
+    ##          </contributor>
+    ## published:Contains the time of the initial creation or 
+    ##          first availability of the entry.
+    ##          <published>2003-12-13T09:17:51-08:00</published>
+    ## source:  If an entry is copied from one feed into another feed ...
+    ##          <source>
+    ##            <id>http://example.org/</id>
+    ##            <title>Fourty-Two</title>
+    ##            <updated>2003-12-13T18:30:02Z</updated>
+    ##            <rights> &amp;copy 2011 Splunk, Inc.</rights>
+    ##          </source>
+    ## rights:  Conveys information about rights, e.g. copyrights, held in 
+    ##          and over the feed.
+    ##          <rights type="html">
+    ##            &amp;copy; 2005 Monty Python
+    ##          </rights>
+
+    return xml_text
+
 def fix_xml(xml_text):
     """ fixup broken XML """
 
-    ## this function detcts broken XML and fixes it up.
+    ## this function detects broken XML and fixes it up.
     ## using emprical evidence, fix up things we have 
     ## seen before as broken XML
 
@@ -83,7 +220,7 @@ def fix_xml(xml_text):
             next_index = xml_text.find(result_preview, index+1)
             if next_index > 0:
                 # build outer wrapper
-                fixed_xml = xml_decl
+                fixed_xml = xml_decl + "\n"
                 fixed_xml += outer_wrapper_start
                 fixed_xml += xml_text.replace(xml_decl, "", 1)
                 fixed_xml += outer_wrapper_end
@@ -92,47 +229,97 @@ def fix_xml(xml_text):
 
     return fixed_xml
 
+def debug_connect(environ):
+    """ optionally print some debug info on connection by client """
+
+    # conditionally generate debug printing
+    debugdata = DEBUG_TEMPLATE % {
+      'python_version': sys.version,
+      'platform': sys.platform,
+      'abs_path': os.path.abspath('.'),
+      'filename': __file__,
+      'python_path': repr(sys.path),
+      'wsgi_env': '\n'.join([ROW_DATA % item for item in environ.items()]),
+    }
+
+    trace("Context data:\n%s\n" % debugdata)
+
+def wait_for_search(context, url):
+    """ when POSTing, wait for a finished splunk response """
+
+    trace("wait_for_search: %s, %s" % (context, url))
+
+    ## if posting, wait on the object to be finished
+    while True:
+        data = context.get(url)
+        pxml = xml.dom.minidom.parseString(data.body.read())
+        for key in pxml.getElementsByTagName("s:key"):
+            if key.getAttribute("name") == "isDone":
+                if key.firstChild.nodeValue == "1":
+                    return
+        time.sleep(1)
+
+def post_query(context, endpoint, query):
+    """ post a query, wait for response """
+
+    trace("post_query : %s, %s, %s" % (context, endpoint, query))
+
+    # remove double search
+    query = query.replace("search=", "", 1)
+
+    sid_xml_text = context.post(endpoint, search=query).body.read()
+    sid_xml = xml.dom.minidom.parseString(sid_xml_text)
+    sid = str(sid_xml.getElementsByTagName("sid")[0].firstChild.nodeValue)
+
+    endpoint = endpoint + "/" + sid
+    wait_for_search(context, endpoint)
+
+    # search has completed, get the data and return
+    endpoint = endpoint + "/results"
+    return context.get(endpoint, output_mode="atom")
+
 def application(environ, start_response):
     """ The splunk proxy processor """
 
-    # conditionally generate debug printing
-    if DEBUG:
-        debugdata = DEBUG_TEMPLATE % {
-          'python_version': sys.version,
-          'platform': sys.platform,
-          'abs_path': os.path.abspath('.'),
-          'filename': __file__,
-          'python_path': repr(sys.path),
-          'wsgi_env': '\n'.join([ROW_DATA % item for item in environ.items()]),
-        }
-
-        ## debug print 
-        FD.write("Context data:\n%s\n" % debugdata)
+    debug_connect(environ)
 
     ## extract some basic HTTP/WSGI info
     endpoint = environ["PATH_INFO"]
     query = environ["QUERY_STRING"]
-    operation = environ["REQUEST_METHOD"]    # GET, POST, PUT, etc 
 
     ## perform idempotent login/connect -- get login creds from ~/.splunkrc
-    opts = cmdopts.parser().loadrc(".splunkrc").parse(sys.argv[1:]).result
-    connection = connect(**opts.kwargs)
+    connection = connect(**(cmdopts.parser().loadrc(".splunkrc")
+                            .parse(sys.argv[1:]).result).kwargs)
+    # get lower level context
+    context = binding.connect( host=connection.host, 
+                               username=connection.username,
+                               password=connection.password)
 
-    ## here we can/should/must look up the endpoint and decide what operation 
-    ## needs to be done -- for now we simply "get" (wkcfix)
-
+    ## here we can/should/must look up the endpoint and decide what operation
+    ## needs to be done -- for now we simply "get" for basic urls, and 
+    ## look for a special "search" in the query (if present) and build a job 
+    ## out of it.
+    ## 
+    ## in particular, we want to look for:
+    ##
+    ## /services/search/jobs
+    
     ## sanitize query, and issue
+    ##
     ## this is a little awkward, browsers and BI apps seem to sanitize the 
     ## query string(s) which doesn't get accepted by splunkd. So we unquote
     ## the original and rebuild it the way we would like to see it.
     if query:
         query = urllib.unquote(query)
-        query = urllib.quote_plus(query)
-        query = query.replace("%3D", "=", 1)
-        final = endpoint + "?" + query
+        ## wkcfix -- break out ? <something> = and use <something> 
+        ## as keyword, not "search"?
+
+        if endpoint == "/services/search/jobs":
+            data = post_query(context, endpoint, query)
+        else:
+            data = context.get(endpoint, search=query) 
     else:
-        final = endpoint
-    data = connection.get(final) # output mode? (wkcfix: brad to investigate sdk support)
+        data = context.get(endpoint) 
 
     ## extract the status and headers from the splunk operation 
     status = str(data["status"]) + " " + data["reason"]
@@ -146,17 +333,14 @@ def application(environ, start_response):
     ## start the response (retransmit the status and headers)
     start_response(status, headers)
 
-    ## follow with the body of the data
-    ## Here is where we can/should/must translate the 
-    ## atom/odata format to ... atom (wkcfix)
+    body = data.body.read()
 
-    if DEBUG:
-        FD.write("http request %s to %s\n" % (operation, final))
-        FD.write("Return data body:\n")
-        FD.write(data["body"] + "\n")
-        FD.flush()
+    # if XML is broken, will attempt to fixup
+    # otherwise it is left alone
+    body = fix_xml(body)
 
-    body = fix_xml(data["body"])
+    trace("Return data body:\n")
+    trace(body + "\n")
 
     return [body]
 
