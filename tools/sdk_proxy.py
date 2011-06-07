4874e1f456feeb1ceb7e4e6813423dbff9c6f8fd
Wim Colgate <wcolgate@splunk.com>
2011-06-07 16:36:48 -0700

    update copyright and apache license format

--- tools/sdk_proxy.py
+++ tools/sdk_proxy.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
+#
+# Copyright 2011 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"): you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+# 
+
 """
 This software embodies a splunkd proxy that translates communication
 between a client application (browser, BI application, etc) and splunkd
-
- Copyright 2011 Splunk, Inc.
-
- Licensed under the Apache License, Version 2.0 (the "License"): you may
- not use this file except in compliance with the License. You may obtain
- a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- License for the specific language governing permissions and limitations
- under the License.
-
-This was originally a sample WSGI application:
-@copyright: 2008 by MoinMoin:ThomasWaldmann
-@license: Python License, see LICENSE.Python for details.
-
 """
 
 # installation support files
