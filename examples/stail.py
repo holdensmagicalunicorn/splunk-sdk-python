1e3d93324ab49e31958bdb6396826e53ee7b3a00
Brad Lovering <blovering@splunk.com>
2011-08-05 11:37:57 -0700

    Streamline stail slightly and make it a closer parallel to follow.py - stail tails a search using export and follow tails using the jobs endpoints.

--- examples/stail.py
+++ examples/stail.py
@@ -14,91 +14,46 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-"""An example to tail an index, with optional specific search."""
+"""Tails a realtime search using the export endpoint and prints results to
+   stdout."""
 
+from pprint import pprint
 import sys
-import xml.dom.minidom
 
 from splunk.client import connect
-from utils import parse
-import splunk.results 
+import splunk.results as results
 
-CLI_RULES = {
-   'search': {
-        'flags': ["--search"],
-        'default': None,
-        'help': "Optional search query string"
-    }
-}
-
-def tail(service, opts):
-    """Tail the index and optional search, via splunk real-time search."""
-
-    # index is the only naked argument
-    iname = opts.args[0]
-
-    # if user supplied a query string, extract it, else leave blank 
-    squery = ""
-    if opts.kwargs['search'] is not None:
-        squery = opts.kwargs['search']
-
-    try:
-        # go until we hit a control-c to exit
-        while True:
-            # tap the export exndpoint
-            # Nota Bene: because this example uses the export endpoint,
-            # only non-transformed searches are supported
-            result = service.get("search/jobs/export",
-                              search="search %s index=%s" % (squery, iname),
-                              earliest_time="rt", 
-                              latest_time="rt", 
-                              search_mode="realtime")
-
-            if result.status != 200:
-                continue
-            # use the reader class to extract the event data
-            reader = splunk.results.ResultsReader(result.body)
-            while True:
-                kind = reader.read()
-                if kind == None:
-                    break
-                if kind == splunk.results.RESULT:
-                    # the data result is going to be an XML fragment, so use 
-                    # an xml parser to extract just the text portion of the 
-                    # "_raw" field which is the raw data of the event. "v"
-                    # is the value portion of the k/v pairing of the splunk
-                    # output
-                    domobj = xml.dom.minidom.parseString(reader.value['_raw']).\
-                             getElementsByTagName("v")[0]
-                    print domobj.firstChild.nodeValue
-
-    except KeyboardInterrupt:
-        print
-        print "Keyboard interrupt  ... exiting"
-    except:
-        print 
-        print "got an unexpected exception ... exiting"
-        print
-        raise
+import utils
 
 def main():
-    """Main program."""
+    usage = "usage: %prog <search>"
+    opts = utils.parse(sys.argv[1:], {}, ".splunkrc", usage=usage)
 
-    usage = "usage: %prog INDEX [<args>]"
+    if len(opts.args) != 1:
+        utils.error("Search expression required", 2)
+    search = opts.args[0]
 
-    argv = sys.argv[1:]
-
-    # parse args, connect and setup 
-    opts = parse(argv, CLI_RULES, ".splunkrc", usage=usage)
     service = connect(**opts.kwargs)
 
-    if len(opts.args) == 0:
-        print "You must supply at the very least an INDEX"
-        sys.exit(1)
+    try:
+        result = service.get(
+            "search/jobs/export",
+            search=search,
+            earliest_time="rt", 
+            latest_time="rt", 
+            search_mode="realtime")
 
-    tail(service, opts)
+        reader = results.ResultsReader(result.body)
+        while True:
+            kind = reader.read()
+            if kind == None: break
+            if kind == results.RESULT:
+                event = reader.value
+                pprint(event)
+
+    except KeyboardInterrupt:
+        print "\nInterrupted."
 
 if __name__ == "__main__":
     main()
-
 
